5d537ffb7a9f219fbb919fb8d79ad882a812c662
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-07 18:27:57 +0530

    [SaiVijayaKumar] Merging to Master

---
+++ StockManagement/IStockMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+
+namespace StockManagement
+{
+    interface IStockMethods
+    {
+        void ShowDetails(List<StockDetails.Stockc> stocksList);
+        void EachShareValue(List<StockDetails.Stockc> stocksList);
+        void TotalShareValue(List<StockDetails.Stockc> stockList);
+    }
+}
