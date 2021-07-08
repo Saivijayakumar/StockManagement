c009e28b3fe128b0b5e714f0409dc77048a806e6
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-08 21:08:30 +0530

    [SaiVijayaKumar] Commiting Upto Buy and sell

--- StockManagement/IStockMethods.cs
+++ StockManagement/IStockMethods.cs
@@ -8,5 +8,10 @@ namespace StockManagement
         void ShowDetails(List<StockDetails.Stockc> stocksList);
         void EachShareValue(List<StockDetails.Stockc> stocksList);
         void TotalShareValue(List<StockDetails.Stockc> stockList);
+        void AddStock(List<StockDetails.Stockc> stockList);
+        void RemoveStock(List<StockDetails.Stockc> stockList);
+        void UsersStocks(List<StockDetails.UserStock> userStocksList);
+        void BuyStock(string patthref);
+        void SellStocks(string pathref);
     }
 }
